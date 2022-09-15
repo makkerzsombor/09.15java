189b66a2d17defc7182fea4a9e9cd64df0f4fc89
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-15 09:35:30 +0200

    Legtavolabb eso pont

--- src/hu/petrik/pontoop/Main.java
+++ src/hu/petrik/pontoop/Main.java
@@ -5,8 +5,8 @@ import hu.petrik.pontoop.pont.Pont;
 public class Main {
     public static void main(String[] args) {
         Pont p1 = new Pont();
-        Pont p2 = new Pont(0,0);
-        Pont p3 = new Pont(3,4);
+        Pont p2 = new Pont(0, 0);
+        Pont p3 = new Pont(3, 4);
         Pont p4 = new Pont(100);
 
         System.out.println(p1);
@@ -19,9 +19,18 @@ public class Main {
             pontok[i] = new Pont(10);
         }
 
-        for (Pont p: pontok){
+        for (Pont p : pontok) {
             System.out.println(p);
         }
-        
+
+        //Origotól legtávolabb eső pont
+        int legtavolabbindex = 0;
+        for (int i = 0; i < pontok.length; i++) {
+            if (pontok[legtavolabbindex].getOrigotolMertTavolsag() < pontok[i].getOrigotolMertTavolsag()) {
+                legtavolabbindex = i;
+            }
+        }
+        System.out.printf("A legtávolabbi pont az origotól a %d. pont, koordinátái: %s, origotól mért távolsága: %f "
+                ,(legtavolabbindex+1), pontok[legtavolabbindex], pontok[legtavolabbindex].getOrigotolMertTavolsag());
     }
 }
