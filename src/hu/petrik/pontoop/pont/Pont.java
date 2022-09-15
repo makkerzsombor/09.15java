6a5cf653be4484696b30a1155f52657aedeeca20
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-15 08:46:01 +0200

    Pont osztaly

---
+++ src/hu/petrik/pontoop/pont/Pont.java
@@ -0,0 +1,16 @@
+package hu.petrik.pontoop.pont;
+
+public class Pont {
+    private int x;
+    private int y;
+
+    public Pont() {
+        x = 0;
+        y = 0;
+    }
+
+    public Pont(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
